f83424e3eefd9b54c5f51473da6e4f813965bebd
Nozdi <nozdi@Nozdi>
2012-06-28 20:12:19 +0200

    init

---
+++ PyGitGui/main/main.py
@@ -0,0 +1,8 @@
+import sys
+from PyQt4 import QtGui
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    w = QtGui.QMessageBox()
+    w.setText("HAHAHA")
+    w.show()
+    sys.exit(app.exec_())
