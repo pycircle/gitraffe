4053e22a3ae740d65b6bd5ede453a4b9fd22f82f
Michał Rostecki <michal.rostecki@gmail.com>
2012-07-09 23:04:28 +0200

    removing old main

--- gitraffe/main/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt4 import QtGui
-
-if __name__ == '__main__':
-    app = QtGui.QApplication(sys.argv)
-    w = QtGui.QMessageBox()
-    w.setText("HAHAHA")
-    w.show()
-    sys.exit(app.exec_())
