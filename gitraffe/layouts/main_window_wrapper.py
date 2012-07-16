208e8eb75d414b6121c09f009862d54b80e87463
Nozdi <nozdi@nozdi-K53SV.(none)>
2012-07-16 19:23:10 +0200

    Delete button, bug repaired

--- gitraffe/layouts/main_window_wrapper.py
+++ gitraffe/layouts/main_window_wrapper.py
@@ -1,5 +1,5 @@
 from PyQt4.QtGui import QMainWindow, QFileDialog, qApp, QListWidgetItem, QMessageBox, QInputDialog, QIcon
-from PyQt4.QtCore import QDir, QObject, SIGNAL
+from PyQt4.QtCore import QDir, QObject, SIGNAL, Qt
 from PyQt4 import QtGui
 from layouts.main_window import Ui_MainWindow
 from git import check_repository
@@ -15,6 +15,7 @@ class MainWindowWrapper(QMainWindow):
         QObject.connect(self.ui.actionAdd_existing_repository, SIGNAL('triggered()'), self.browse)
         QObject.connect(self.ui.actionAdd_existing_repository_2, SIGNAL('triggered()'), self.browse)
         QObject.connect(self.ui.actionExit, SIGNAL('triggered()'), qApp.exit)
+        QObject.connect(self.ui.actionDelete_repository, SIGNAL('triggered()'), self.delete_listWidgetitem)
     def browse(self):
         directory = QFileDialog.getExistingDirectory(self, QDir.homePath(), QDir.homePath())
         if directory!="" and check_repository(directory):
@@ -23,4 +24,15 @@ class MainWindowWrapper(QMainWindow):
             if name[1]:
                 repo = QListWidgetItem(QIcon(os.path.dirname(main_window.__file__)+'/icons/Git-Icon-Black.png'), name[0], self.ui.listWidget)
                 repo.setStatusTip(directory)
+                repo.setFlags(repo.flags() | Qt.ItemIsEditable)
+        elif directory=="": pass
         else: QMessageBox.critical(self, "Error", "That directory is not a git repository", QMessageBox.Ok)
+    def delete_listWidgetitem(self):
+        if self.ui.listWidget.count()!=0:
+            respond = QMessageBox.question(self, "Delete", 
+                                           "Are you sure, that you want delete " + self.ui.listWidget.item(self.ui.listWidget.currentRow()).text(),
+                                           QMessageBox.Ok, QMessageBox.Cancel)
+            if respond==QMessageBox.Ok:
+                self.ui.listWidget.takeItem(self.ui.listWidget.currentRow())
+    
+    
